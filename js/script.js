3da5b6943e5c6f6efaf1538e90b107d92194ab55
Harshdeep <w0788912@myscc.ca>
2022-12-11 21:08:51 -0500

    Added Validation

--- js/script.js
+++ js/script.js
@@ -10,15 +10,9 @@
     form.addEventListener('submit', event => {
       event.preventDefault()
       event.stopPropagation()
-      if (form.checkValidity()) {
-        // validation passed, so we can open a modal
-        // This link leads to the documentation on how to create a modal in JavaScript
-        // https://getbootstrap.com/docs/5.2/components/modal/#via-javascript
-        // Given the code found there, first create your new instance of a Modal, by selecting your pages HTML modal (you don't need to pass in options)
-        
-
-        // then open it by using the show() method
-
+      if (!form.checkValidity()) {
+        event.preventDefault()
+        event.stopPropagation()
       }
 
       
